ab3288666449572f7ad8dd961c600845cbade2ec
Vhairob Roy <vhairobroy96@.com>
2025-08-29 21:44:37 +0600

    My Seccond commit

--- node.js
+++ node.js
@@ -6,3 +6,25 @@
       heartCount++;
       document.getElementById("heartCount").innerText = heartCount;
     }
+
+     function copyNumber(num) {
+      navigator.clipboard.writeText(num);
+      alert("Copied: " + num);
+      copyCount++;
+      document.getElementById("copyCount").innerText = copyCount;
+    }
+
+    function makeCall(name, num) {
+      if (coinCount < 20) {
+        alert("Not enough coins to make a call!");
+        return;
+      }
+      alert("Calling " + name + " at " + num);
+      coinCount -= 20;
+      document.getElementById("coinCount").innerText = coinCount;
+
+      let now = new Date().toLocaleTimeString();
+      let li = document.createElement("li");
+      li.textContent = `${name} (${num}) - ${now}`;
+      document.getElementById("historyList").appendChild(li);
+    }
