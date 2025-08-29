686d833a8b20085b0166206507beefde2f001b35
Vhairob Roy <vhairobroy96@.com>
2025-08-29 20:15:44 +0600

    first commit

---
+++ node.js
@@ -0,0 +1,8 @@
+   let heartCount = 0;
+    let coinCount = 100;
+    let copyCount = 0;
+
+    function increaseHeart() {
+      heartCount++;
+      document.getElementById("heartCount").innerText = heartCount;
+    }
